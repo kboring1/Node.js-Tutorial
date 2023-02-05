a64a672751b1489141dc92238bc27951fcc668ca
kboring1 <kboring1@ivytech.edu>
2023-02-04 22:24:27 -0500

    #

--- file.js
+++ file.js
@@ -1,19 +1,45 @@
 //reading files
 const fs = require('fs');
 
-fs.readFile('./docs/clog1.txt', (err,data)=>{
-    if(err) {
-        console.log(err);
-    }
-    console.log(data.toString());
-    })
+//fs.readFile('./docs/blog1.txt', (err,data)=>{
+ //   if(err) {
+ //       console.log(err);
+ //   }
+ //   console.log(data.toString());
+ //   })
     
 
+//console.log('last line');
 //writing files
-
+//fs.writeFile('./docs/blog1.txt', 'hello World', () =>{
+//    console.log('file was written')
+//})
 
 //directories
-
+if(!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) =>{
+    if(err){
+        console.log(err);
+    }
+    console.log('folder created');
+});
+} else {
+    fs.rmdir('./assets', (err) =>{
+        if(err){
+            console.log(err)
+        }
+        console.log('folder deleted')
+    })
+}
 
 
 //deleting files
+if(fs.existsSync('./docs/deleteme.txt')){
+    fs.unlink('./docs/deleteme.txt', (err) =>{
+        if(err){
+            console.log(err)
+        }
+        console.log('files deleted');
+    })
+
+}
