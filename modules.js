802ebdaf08e66fd5bd5f345100fb0d88b15a8fdb
kboring1 <kboring1@ivytech.edu>
2023-02-04 22:24:16 -0500

    made changes

--- modules.js
+++ modules.js
@@ -1,3 +1,7 @@
-const xyz = require('./people');
+const { people, ages } = require('./people');
 
-console.log(xyz);
+console.log(people,ages);
+
+const os = require('os');
+
+console.log(os.platform(),os.homedir());
